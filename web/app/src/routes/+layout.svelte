9f11f9a1958e3e14e2cff2cb8831e6aeff4f2416
7sDream <i@7sdre.am>
2024-02-28 23:08:03 +0800

    feat(webapp): add Google Analytics script

--- web/app/src/routes/+layout.svelte
+++ web/app/src/routes/+layout.svelte
@@ -1,8 +1,35 @@
-<script>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { PUBLIC_GOOGLE_ANALYTICS_ID } from '$env/static/public';
 	import '../app.pcss';
 
 	import { ModeWatcher } from 'mode-watcher';
+
+	if (browser) {
+		if (PUBLIC_GOOGLE_ANALYTICS_ID) {
+			(window as any).dataLayer = (window as any).dataLayer || [];
+
+			function gtag(..._args: any[]) {
+				(window as any).dataLayer.push(arguments);
+			}
+
+			gtag('js', new Date());
+			gtag('config', PUBLIC_GOOGLE_ANALYTICS_ID);
+			console.info('Google Analytics initialized.');
+		} else {
+			console.debug('NO Google Analytics ID provided.');
+		}
+	}
 </script>
+
+<svelte:head>
+	{#if PUBLIC_GOOGLE_ANALYTICS_ID}
+		<script
+			async
+			src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_GOOGLE_ANALYTICS_ID}"
+		></script>
+	{/if}
+</svelte:head>
 
 <ModeWatcher />
 <slot />
